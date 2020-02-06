eaaf0e01f08b5be69667406bda969fb326a0ed6a
subtleasian <danielmson21@gmail.com>
2020-02-05 17:03:48 -0800

    completed basic app functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,50 +10,55 @@ var numCorrect = 0;
 var numIncorrect = 0;
 var numUnanswered = 0;
 
-var timeRemaining = 30;
+var timeRemaining = 10;
 var intervalId;
 
 function checkAnswers() {
     // check q1
-    if ($("#q1-option1").checked === true) {
+    if (document.getElementById("q1-option1").checked === true) {
         numIncorrect++;
-    } else if ($("#q1-option2").checked === true) {
+    } else if (document.getElementById("q1-option2").checked === true) {
         numCorrect++;
     } else {
         numUnanswered++;
     }
     // check q2
-    if ($("#q2-option1").checked === true) {
+    if (document.getElementById("q2-option1").checked === true) {
         numCorrect++;
-    } else if ($("#q2-option2").checked === true) {
+    } else if (document.getElementById("q2-option2").checked === true) {
         numIncorrect++;
     } else {
         numUnanswered++;
     }
     // check q3
-    if ($("#q3-option1").checked === true) {
+    if (document.getElementById("q3-option1").checked === true) {
         numIncorrect++;
-    } else if ($("#q3-option2").checked === true) {
+    } else if (document.getElementById("q3-option2").checked === true) {
         numCorrect++;
     } else {
         numUnanswered++;
     }
     // check q4
-    if ($("#q4-option1").checked === true) {
+    if (document.getElementById("q4-option1").checked === true) {
         numIncorrect++;
-    } else if ($("#q4-option2").checked === true) {
+    } else if (document.getElementById("q4-option2").checked === true) {
         numCorrect++;
     } else {
         numUnanswered++;
     }
     // check q5
-    if ($("#q5-option1").checked === true) {
+    if (document.getElementById("q5-option1").checked === true) {
         numIncorrect++;
-    } else if ($("#q5-option2").checked === true) {
+    } else if (document.getElementById("q5-option2").checked === true) {
         numCorrect++;
     } else {
         numUnanswered++;
     }
+
+    $("#num-correct").text(numCorrect);
+    $("#num-incorrect").text(numIncorrect);
+    $("#num-unanswered").text(numUnanswered);
+
 }
 
 function run() {
@@ -66,10 +71,14 @@ function decrement() {
     if (timeRemaining === 0) {
         clearInterval(intervalId);
         alert("Time is up!");
+        checkAnswers();
+        $("#trivia-box").hide();
+        $("#results-box").css("display", "block");
     }
 }
 
 $("#start-btn").on("click", function() {
     run();
-    
+    $("#start-box").hide();
+    $("#trivia-box").css("display", "block");
 });
